01ea8253efdbecaab4b18b03ee14a03d0cf8b1d8
Valk-db <sivalkmedia@gmail.com>
2026-07-22 21:35:15 -0400

    Integrate mlx-swift-lm, add AdapterManager, and update client simulator

--- Package.swift
+++ Package.swift
@@ -1,17 +1,40 @@
-// swift-tools-version:5.9
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
-    name: "SwairmClient",
-    platforms: [.macOS(.v13)],
+    name: "Swairm",
+    platforms: [
+        .macOS(.v14),
+        .iOS(.v17)
+    ],
+    products: [
+        .library(name: "SwairmCore", targets: ["SwairmCore"]),
+        .executable(name: "swairm-client", targets: ["swairm-client"])
+    ],
     dependencies: [
-        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.16"),
+        .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.10.0"),
+        .package(url: "https://github.com/ml-explore/mlx-swift-lm", from: "3.31.3"),
+        .package(url: "https://github.com/ZIPFoundation/ZIPFoundation", from: "0.9.19")
     ],
     targets: [
         .target(
             name: "SwairmCore",
-            dependencies: [.product(name: "ZIPFoundation", package: "ZIPFoundation")]),
-        .executableTarget(name: "swairm-client", dependencies: ["SwairmCore"]),
-        .testTarget(name: "SwairmCoreTests", dependencies: ["SwairmCore"]),
+            dependencies: [
+                .product(name: "MLX", package: "mlx-swift"),
+                .product(name: "MLXNN", package: "mlx-swift"),
+                .product(name: "MLXOptimizers", package: "mlx-swift"),
+                .product(name: "MLXLinalg", package: "mlx-swift"),
+                .product(name: "MLXLMCommon", package: "mlx-swift-lm"),
+                "ZIPFoundation"
+            ]
+        ),
+        .executableTarget(
+            name: "swairm-client",
+            dependencies: ["SwairmCore"]
+        ),
+        .testTarget(
+            name: "SwairmCoreTests",
+            dependencies: ["SwairmCore"]
+        )
     ]
 )
