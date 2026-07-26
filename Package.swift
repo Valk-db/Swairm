917cec1c4b9b83aab59ba245cf428c5fe27c56b9
Valk-db <sivalkmedia@gmail.com>
2026-07-26 05:08:45 -0400

    Fix: pass -Wno-deprecated-declarations via -Xcc for C compiler

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
                 "ZIPFoundation"
             ],
             swiftSettings: [
-                .unsafeFlags(["-Wno-deprecated-declarations"])
+                .unsafeFlags(["-Xcc", "-Wno-deprecated-declarations"])
             ]
         ),
         .executableTarget(
