4558d111e542baec8e5284929b53d03fbd6ab002
Valk-db <sivalkmedia@gmail.com>
2026-07-27 19:23:03 -0400

    Fix CI warnings: suppress cblas_sgemm deprecation with Accelerate defines, mark MLXTrainer closure @Sendable

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
                 "ZIPFoundation"
             ],
             swiftSettings: [
-                .unsafeFlags(["-Xcc", "-Wno-deprecated-declarations"])
+                .unsafeFlags(["-Xcc", "-Wno-deprecated-declarations", "-Xcc", "-DACCELERATE_NEW_LAPACK", "-Xcc", "-DACCELERATE_LAPACK_ILP64"])
             ]
         ),
         .executableTarget(
